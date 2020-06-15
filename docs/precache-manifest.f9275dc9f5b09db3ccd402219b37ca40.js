7c06b442c7aaeee134d85bb45f99fd8cf2ec4efc
beatriz-perez <beatriz.pfp@gmail.com>
2020-06-15 10:21:47 +0200

    rebuild docs folder

---
+++ docs/precache-manifest.f9275dc9f5b09db3ccd402219b37ca40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25fda927a7edffdc01a683661b5de03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "65e2c78e74ac0e7ce8c1",
+    "url": "./static/css/main.841eeca0.chunk.css"
+  },
+  {
+    "revision": "213043ac7a28916a7378",
+    "url": "./static/js/2.ed88633d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ed88633d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65e2c78e74ac0e7ce8c1",
+    "url": "./static/js/main.1e437b04.chunk.js"
+  },
+  {
+    "revision": "afce45393a8b309115de",
+    "url": "./static/js/runtime-main.ede1451e.js"
+  }
+]);
